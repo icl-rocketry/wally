832f98a07329f7b7b50058bbfb7adc7a630d0e0c
xxCamelot <90185538+xxCamelot@users.noreply.github.com>
2023-12-12 20:02:49 +0000

    Points in a plot for test rocket

--- testdatagen.py
+++ testdatagen.py
@@ -1,27 +1,27 @@
-#%%
-
-import plotly.graph_objects as go
+import plotly.graph_objs as go
 import numpy as np
 
-# Define the parabolic function
-def parabolic_function(x, y):
-    return x**2 + y**2
+# Define the range for the variable axis
+x = np.arange(-10, 10.5, 0.5)
+y = np.zeros_like(x)
 
-# Generate data
-x = np.linspace(-5, 5, 100)
-y = np.linspace(-5, 5, 100)
-x, y = np.meshgrid(x, y)
-z = parabolic_function(x, y)
+# Define the equation for the positive half of the 2D parabola
+z_max = 100
+z = z_max - x**2
+z = z * (z >= 0)  # To remove negative parts
 
-# Create a 3D surface plot using Plotly
-fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
+# Create a 3D scatter plot for the points along the parabola
+fig = go.Figure(data=[go.Scatter3d(x=x, y=y, z=z, mode='markers', marker=dict(color='blue', size=5))])
 
-# Set layout
-fig.update_layout(scene=dict(
-                    xaxis_title='X',
-                    yaxis_title='Y',
-                    zaxis_title='Z'),
-                  )
+# Update layout and axes labels
+fig.update_layout(
+    title='Points Along Positive Half of 2D Parabola in 3D Space (Interval 0.5 in x, y, and z)',
+    scene=dict(
+        xaxis=dict(title='X-axis'),
+        yaxis=dict(title='Y-axis'),
+        zaxis=dict(title='Z-axis'),
+    )
+)
 
 # Show the plot
 fig.show()
