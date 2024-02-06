b95ff9e0768ecb1b9fb52f81102a07db80f2953d
xxCamelot <90185538+xxCamelot@users.noreply.github.com>
2024-02-06 19:53:13 +0000

    final problem

--- testdatagen.py
+++ testdatagen.py
@@ -7,6 +7,12 @@ from sympy import Symbol, Plane, Point3D, Line3D#, Normal_Vector
 # pip install vector 
 import vector 
 
+def upper_tri_masking(A):
+    m = A.shape[0]
+    r = np.arange(m)
+    mask = r[:,None]<r
+    return A[mask]
+
 # Define the range for the variable axis
 x = np.arange(-100, 105, 5)
 y = np.zeros_like(x)
@@ -57,12 +63,11 @@ for i in range(len(x_gs)):
 # Reconstructing the trajectory from the time received. 
 # The difference in the signals shows the distance of the rocket at a given point in time.
 gs_dis_new = np.zeros((len(x),len(x_gs)))
-d12 = np.zeros((len(x_gs),len(x_gs)))
-v12 = np.zeros((len(x_gs),len(x_gs)))
-d_coord = np.zeros((len(x_gs),len(x_gs)))
-
 
 for i in range(len(x)): # values of time. 
+
+    planes = np.zeros((len(x_gs),len(x_gs)), dtype=Plane)
+
     for j in range(len(x_gs)): # Iterating over the number of ground stations
         # Calculating the distance between each ground station and target (for each time)
         gs_dis_new[i][j] = (time_signals_recieved[i][j] - time[i])*299792458
@@ -83,6 +88,28 @@ for i in range(len(x)): # values of time.
                 # Coordinates on the intersection circle.
                 # d_coord[k][l] = v12[k][l]*(d12[k][l]/v[k][l]) + (x_gs[k],y_gs[k],z_gs[k])
                 d_coord = v12*(d12/v[k][l]) + [x_gs[k],y_gs[k],z_gs[k]] # this needs to be stored in an array. 
+                
+                planes[k][l] = Plane(Point3D(d_coord), normal_vector=(v12))
+
+    
+
+    newplanes = upper_tri_masking(planes)
+
+    points = np.zeros((len(newplanes),len(newplanes),len(newplanes)))
+
+    for n in range(len(newplanes)): 
+        for o in range(len(newplanes)):
+            for p in range(len(newplanes)):
+                if newplanes[n] == newplanes[o] or newplanes[n] == newplanes[p] or newplanes[p] == newplanes[o]:
+                    points[n][o][p] = 0
+                else:
+                    linetemp = newplanes[n].intersection(newplanes[o])
+                    print(linetemp[0])
+                    print(linetemp[0].intersection(newplanes[p]))
+                    
+
+
+                
 
     # Calculating the coordinates of the d value. 
                 
@@ -93,3 +120,4 @@ for i in range(len(x)): # values of time.
                 # Calculated the point on the intersection plane. Tried to calculate the coordinates of the point 
                 # on the intersection plane - need this and the normal vector to continue. - sharwa has a function for this. 
                 # to continue to do plane calculations - might need to use algebraic/ plane toolbox. 
+
