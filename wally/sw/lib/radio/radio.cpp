41e7f9030ffa5ce8fdec1e3eee8e2cf3736d6e28
Shrey Bohra <42218014+shreybohra@users.noreply.github.com>
2024-06-24 00:59:23 +0100

    init function with distinct spi bus

--- wally/sw/lib/radio/radio.cpp
+++ wally/sw/lib/radio/radio.cpp
@@ -1,20 +1,23 @@
 #include "radio.h"
+#include <RadioLib.h>
+#include <Arduino.h>
+#include <SPI.h>
 
 #define FREQ 915.0
 #define SPREADING_FACTOR 12
-#define BANDWIDTH 500
+#define BANDWIDTH 125
 #define CODING_RATE 5
 #define SYNC_WORD 0x34
 #define TX_POWER 22
 #define PREAMBLE_LENGTH 20
 
 
-Radio::Radio() {
-    // empty constructor    
-}
+int Radio::init(SPIClass rad_spi, float frequency, int spreadingFactor, int bandwidth, int codingRate, int syncWord){
+    mod = new Module(RAD_CS, RAD_DIO1, RAD_RST, RAD_BUSY, rad_spi);
+    radio = SX1262(mod);
 
-bool Radio::init(float frequency, int spreadingFactor, int bandwidth, int codingRate, int syncWord){}
-    SX1262 radio = new Module(CS_PIN, DIO1_PIN, RST_PIN);
     state = radio.begin(FREQ, SPREADING_FACTOR, BANDWIDTH, CODING_RATE, SYNC_WORD);
+
+    return state;
 }
 
--- wally/sw/lib/radio/radio.h
+++ wally/sw/lib/radio/radio.h
@@ -3,25 +3,21 @@
 
 #include <Arduino.h>
 #include <RadioLib.h>
+#include <SPI.h>
 
-// Define your module specifics
-#define CS_PIN  10
-#define DIO1_PIN 2
-#define RST_PIN 9
-#define FREQUENCY 915.0
 
 class Radio {
 public:
     
     Radio(); // Constructor
-    bool init(float frequency, int spreadingFactor, int bandwidth, int codingRate, int syncWord); // Initialize the radio
+    int init(SPIClass rad_spi, float frequency, int spreadingFactor, int bandwidth, int codingRate, int syncWord); // Initialize the radio
     void transmit(const uint8_t* data, size_t length); // transmit byte array
     size_t receive(uint8_t* buffer, size_t bufferSize);
 
 private:
+    Module* mod;
     SX1262 radio;
     int state;
-
 };
 
 #endif // LORARADIO_H 
