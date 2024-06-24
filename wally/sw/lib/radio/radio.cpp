3a8fcbd5f44e482f298b75213bc1362be278c519
Shrey Bohra <42218014+shreybohra@users.noreply.github.com>
2024-06-24 02:18:44 +0100

    interrupt based transmit

--- wally/sw/lib/radio/radio.cpp
+++ wally/sw/lib/radio/radio.cpp
@@ -11,6 +11,13 @@
 #define TX_POWER 22
 #define PREAMBLE_LENGTH 20
 
+// save transmission state between loops
+int transmissionState = RADIOLIB_ERR_NONE;
+
+// flag to indicate that a packet was sent
+volatile bool transmittedFlag = false;
+
+
 
 int Radio::init(SPIClass rad_spi, float frequency, int spreadingFactor, int bandwidth, int codingRate, int syncWord){
     mod = new Module(RAD_CS, RAD_DIO1, RAD_RST, RAD_BUSY, rad_spi);
@@ -18,6 +25,30 @@ int Radio::init(SPIClass rad_spi, float frequency, int spreadingFactor, int band
 
     state = radio.begin(FREQ, SPREADING_FACTOR, BANDWIDTH, CODING_RATE, SYNC_WORD);
 
+    if (state == RADIOLIB_ERR_NONE) {
+        Serial.println(F("success!"));
+    } else {
+        Serial.print(F("failed, code "));
+        Serial.println(state);
+        
+    }
+
     return state;
 }
 
+void Radio::setFlag(void) {
+  // we sent a packet, set the flag
+  transmittedFlag = true;
+}
+
+void Radio::transmit(uint8_t* data, size_t length){
+    // radio.transmit(data, length);
+
+    if (transmittedFlag == true) {
+        // we sent a packet, reset the flag
+        transmittedFlag = false;
+
+        state = radio.startTransmit(data, length);
+    }
+    
+}
--- wally/sw/lib/radio/radio.h
+++ wally/sw/lib/radio/radio.h
@@ -11,13 +11,16 @@ public:
     
     Radio(); // Constructor
     int init(SPIClass rad_spi, float frequency, int spreadingFactor, int bandwidth, int codingRate, int syncWord); // Initialize the radio
-    void transmit(const uint8_t* data, size_t length); // transmit byte array
+    void transmit(uint8_t* data, size_t length); // transmit byte array
     size_t receive(uint8_t* buffer, size_t bufferSize);
 
 private:
     Module* mod;
     SX1262 radio;
     int state;
+    int transmissionState;
+    volatile bool transmittedFlag;
+    void setFlag(void);
 };
 
 #endif // LORARADIO_H 
